9f07cb94285074e987e0ee4b02886078a49fa99a
Moaaz <moaaz_bs@yahoo.com>
2018-12-25 08:39:53 +0200

    Updates

---
+++ precache-manifest.9eb2ceafda8bf36caf60949b3fe1c367.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b9199120bf9e9506ba91b140a0e681",
+    "url": "/recommended-restaurants/static/media/menu.b6b91991.svg"
+  },
+  {
+    "revision": "9367e4e38fa691cc35e043edc571e282",
+    "url": "/recommended-restaurants/static/media/google.9367e4e3.svg"
+  },
+  {
+    "revision": "dcea39ad03938a4bf1f804706b4280df",
+    "url": "/recommended-restaurants/static/media/foursquare.dcea39ad.svg"
+  },
+  {
+    "revision": "506cc0ee287bc2328597",
+    "url": "/recommended-restaurants/static/js/runtime~main.506cc0ee.js"
+  },
+  {
+    "revision": "f0967cf5c53f529b638d",
+    "url": "/recommended-restaurants/static/js/main.f0967cf5.chunk.js"
+  },
+  {
+    "revision": "9aff8d6064d3faaba019",
+    "url": "/recommended-restaurants/static/js/1.9aff8d60.chunk.js"
+  },
+  {
+    "revision": "f0967cf5c53f529b638d",
+    "url": "/recommended-restaurants/static/css/main.766cdba6.chunk.css"
+  },
+  {
+    "revision": "d5ae3e7da16514a1320cef10ba548ff6",
+    "url": "/recommended-restaurants/index.html"
+  }
+];
