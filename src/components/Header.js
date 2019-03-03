a89558a92b103af82afce68bbd0400e0a3cfbded
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-03 09:07:51 +0200

    Re-arranged header components to be on one line on any screen size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import menuIcon from '../icons/menu.svg';
 
-const Header = props => {
-  const {sidebarShown, modalOpened, openModal, toggleSidebar} = props;
-  return (
-    <header id="main-header" role="banner">
-      <h1>Recommended Restaurants</h1>
-      <button 
-        type="button" 
-        id="menu-btn"
-        onClick={toggleSidebar}
-        aria-controls="sidebar"
-        aria-pressed={sidebarShown ? "true" : "false"}
-        aria-expanded={sidebarShown ? "true" : "false"}
-      >
-        <img 
-          src={menuIcon} 
-          alt="Menu Icon" 
-        />
-      </button>
-      <button
-        type="button"
-        id="place-btn"
-        onClick={openModal}
-        aria-controls="modal"
-        aria-pressed={modalOpened ? "true" : "false"}
-        aria-expanded={modalOpened ? "true" : "false"}
-      >
-        change place
-      </button>
-    </header>
-  );
+class Header extends Component {
+  state = { 
+    smallScreen: null
+  }
+
+  detectScreenWidth = () => {
+    console.log(1);
+    const smallBreakPoint = 528;
+    const screenWidth = window.innerWidth;
+    const {smallScreen} = this.state;
+    if (screenWidth > smallBreakPoint && smallScreen) {
+      this.setState({
+        smallScreen: false
+      })
+    } else if (screenWidth < smallBreakPoint && !smallScreen) {
+      this.setState({
+        smallScreen: true
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.detectScreenWidth();
+    window.addEventListener('resize', this.detectScreenWidth)
+  }
+
+  render() {
+    const {smallScreen} = this.state;
+    const {sidebarShown, modalOpened, openModal, toggleSidebar} = this.props; 
+    return (
+      <header id="main-header" role="banner">
+        <h1>{smallScreen ? 'RR' : 'Recommended Restaurants'}</h1>
+        <button 
+          type="button" 
+          id="menu-btn"
+          onClick={toggleSidebar}
+          aria-controls="sidebar"
+          aria-pressed={sidebarShown ? "true" : "false"}
+          aria-expanded={sidebarShown ? "true" : "false"}
+        >
+          <img 
+            src={menuIcon} 
+            alt="Menu Icon" 
+          />
+        </button>
+        <button
+          type="button"
+          id="place-btn"
+          onClick={openModal}
+          aria-controls="modal"
+          aria-pressed={modalOpened ? "true" : "false"}
+          aria-expanded={modalOpened ? "true" : "false"}
+        >
+          change place
+        </button>
+      </header>
+    );
+  }
 }
 
 export default Header;
